8fd345d5462439cf54ac415355bf4c4a0c852fe3
Julio <juliomoreno7217@gmail.com>
2023-10-01 21:07:53 -0600

    import libraries

--- Tensorflow_Course/HelloWorld.py
+++ Tensorflow_Course/HelloWorld.py
@@ -1,1 +1,11 @@
-print('Hello World')
+try:
+    import numpy
+    import rpa
+    import openpyxl
+    import PIL
+    import pandas
+    import xlwings
+    
+    print('Hellow World Done!')
+except:
+    pass
